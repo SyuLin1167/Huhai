f914556c3824e1ff3085a915bc2f0202b599f412
SyuLin <>
2023-05-20 16:30:59 +0900

    シーンの新規追加

---
+++ SourceCode/Scene/EscapeScene/Escape.cpp
@@ -0,0 +1,1 @@
+#include "Day1Escape.h"
---
+++ SourceCode/Scene/EscapeScene/Escape.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"../SceneBase/SceneBase.h"
+#include"../../Object/ObjectManager/ObjManager.h"
+#include "../../Collision/Collision.h"
+
+/*脱出シーンクラス*/
+class Escape:public ObjectBase
+{
+public:
+    Escape();
+    ~Escape();
+
+};
+
